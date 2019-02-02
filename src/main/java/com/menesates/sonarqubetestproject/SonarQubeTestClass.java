26c9cb4df350e9f12000e6f4e596472c5e2af431
Muhammed Enes Ates <atesmuhammedenes@gmail.com>
2019-02-02 22:04:18 +0300

    first commit

---
+++ src/main/java/com/menesates/sonarqubetestproject/SonarQubeTestClass.java
@@ -0,0 +1,22 @@
+package com.menesates.sonarqubetestproject;
+
+public class SonarQubeTestClass {
+
+    private int a = 5;
+    private int b = 10;
+
+    public void sonarQubeTestMethod(){
+        System.out.println(a);
+    }
+
+    public void sonarQubeTestMethod2(){
+
+    }
+
+    public void sonarQubeTestMethod3(){
+        while (true){
+
+        }
+    }
+
+}
